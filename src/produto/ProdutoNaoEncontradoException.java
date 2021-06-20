f96c683f0a86d8b24fb6c3fe8e9d5acb63ac5a1b
Victor Marques <victyrmarques@gmail.com>
2021-06-20 13:15:28 -0300

    Commit 1 - Inclusão classes presente no repositório https://github.com/vaniacourses/intro_junit com alterações de remoção de warnings

---
+++ src/produto/ProdutoNaoEncontradoException.java
@@ -0,0 +1,11 @@
+package produto;
+
+public class  ProdutoNaoEncontradoException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ProdutoNaoEncontradoException() {
+		super();
+	}
+
+}
